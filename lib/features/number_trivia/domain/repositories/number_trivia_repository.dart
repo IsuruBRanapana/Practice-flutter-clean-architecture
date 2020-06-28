33f966a4300143cc816f5946fdb2a124db3d07f9
Isuru B. Ranapana <raaisurub.ranapana@gmail.com>
2020-06-28 10:51:38 +0530

    all folder structure for test are added

--- lib/features/number_trivia/domain/repositories/number_trivia_repository.dart
+++ lib/features/number_trivia/domain/repositories/number_trivia_repository.dart
@@ -1,3 +1,8 @@
+import 'package:cleanpractice/core/errors/failures.dart';
+import 'package:cleanpractice/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class NumberTriviaRepository{
-  
+  Future<Either<Failure,NumberTrivia>> getConcreteNumberTrivia(int number);
+  Future<Either<Failure,NumberTrivia>> getRandomNumberTrivia();
 }
