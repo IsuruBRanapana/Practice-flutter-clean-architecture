6d59e2fc564d302387a05a33aad793f18f68e2d5
isuru_r <isuru_r@epiclanka.net>
2020-07-07 22:21:30 -0700

    finish remote number trivia with tdd

--- lib/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart
+++ lib/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart
@@ -1,6 +1,11 @@
-import 'package:cleanpractice/features/number_trivia/data/models/number_trivia_model.dart';
+import 'dart:convert';
 
-abstract class NumberTriviaRemoteDataSource{
+import 'package:cleanpractice/core/errors/exceptions.dart';
+import 'package:cleanpractice/features/number_trivia/data/models/number_trivia_model.dart';
+import 'package:meta/meta.dart';
+import 'package:http/http.dart' as http;
+
+abstract class NumberTriviaRemoteDataSource {
   ///calls the http://numbersapi.com/{number} endpoint.
   ///
   /// Throws a [ServerException] for all error codes.
@@ -10,4 +15,31 @@ abstract class NumberTriviaRemoteDataSource{
   ///
   /// Throws a [ServerException] for all error codes.
   Future<NumberTriviaModel> getRandomNumberTrivia();
+}
+
+class NumberTriviaRemoteDataSourceImpl implements NumberTriviaRemoteDataSource {
+  final http.Client client;
+
+  NumberTriviaRemoteDataSourceImpl({@required this.client});
+
+  @override
+  Future<NumberTriviaModel> getConcreteNumberTrivia(int number) =>
+      _getTriviaFromUrl('http://numbersapi.com/$number');
+
+  @override
+  Future<NumberTriviaModel> getRandomNumberTrivia() =>
+      _getTriviaFromUrl('http://numbersapi.com/random');
+
+  Future<NumberTriviaModel> _getTriviaFromUrl(String url) async {
+    final response = await client.get(
+      url,
+      headers: {'Content-Type': 'application/json'},
+    );
+
+    if (response.statusCode == 200) {
+      return NumberTriviaModel.fromJson(json.decode(response.body));
+    } else {
+      throw ServerException();
+    }
+  }
 }
--- test/features/data/datasources/number_trivia_local_data_source_test.dart
+++ test/features/data/datasources/number_trivia_local_data_source_test.dart
@@ -10,47 +10,54 @@ import 'package:matcher/matcher.dart';
 
 import '../../../fixtures/fixture_reader.dart';
 
-class MockSharedPreferences extends Mock implements SharedPreferences{}
+class MockSharedPreferences extends Mock implements SharedPreferences {}
 
-void main(){
+void main() {
   NumberTriviaLocalDataSourceImpl dataSource;
   MockSharedPreferences mockSharedPreferences;
 
-  setUp((){
-    mockSharedPreferences=MockSharedPreferences();
-    dataSource=NumberTriviaLocalDataSourceImpl(sharedPreferences: mockSharedPreferences);
+  setUp(() {
+    mockSharedPreferences = MockSharedPreferences();
+    dataSource = NumberTriviaLocalDataSourceImpl(
+        sharedPreferences: mockSharedPreferences);
   });
 
-  group('getLastNumberTrivia', (){
-    final tNumberTriviaModel=NumberTriviaModel.fromJson(json.decode(fixture('trivia_cached.json')));
-    test('should return NumberTrivia from SharedPreferences when there is one in cache', ()async{
+  group('getLastNumberTrivia', () {
+    final tNumberTriviaModel =
+        NumberTriviaModel.fromJson(json.decode(fixture('trivia_cached.json')));
+    test(
+        'should return NumberTrivia from SharedPreferences when there is one in cache',
+        () async {
       //arrange
-      when(mockSharedPreferences.getString(any)).thenReturn(fixture('trivia_cached.json'));
+      when(mockSharedPreferences.getString(any))
+          .thenReturn(fixture('trivia_cached.json'));
       //act
       final result = await dataSource.getLastNumberTrivia();
       //assert
       verify(mockSharedPreferences.getString('CACHED_NUMBER_TRIVIA'));
       expect(result, equals(tNumberTriviaModel));
     });
-    test('should throw a CacheException when there is not a cached value', ()async{
+    test('should throw a CacheException when there is not a cached value',
+        () async {
       //arrange
       when(mockSharedPreferences.getString(any)).thenReturn(null);
       //act
       final call = dataSource.getLastNumberTrivia;
       //assert
-      expect(()=>call(),throwsA(TypeMatcher<CacheException>()));
+      expect(() => call(), throwsA(TypeMatcher<CacheException>()));
     });
-  }
-  );
+  });
 
-  group('cacheNumberTrivia', (){
-    final tNumberTriviaModel = NumberTriviaModel(number: 1,text: 'Test trivia');
-    test('should call SharedPreferences to cache the data', ()async{
+  group('cacheNumberTrivia', () {
+    final tNumberTriviaModel =
+        NumberTriviaModel(number: 1, text: 'Test trivia');
+    test('should call SharedPreferences to cache the data', () async {
       //act
       dataSource.cacheNumberTrivia(tNumberTriviaModel);
       //assert
-      final expectedJsonString=json.encode(tNumberTriviaModel.toString());
-      verify(mockSharedPreferences.setString(CASHED_NUMBER_TRIVIA, expectedJsonString));
+      final expectedJsonString = json.encode(tNumberTriviaModel.toJson());
+      verify(mockSharedPreferences.setString(
+          CASHED_NUMBER_TRIVIA, expectedJsonString));
     });
   });
-} 
+}
