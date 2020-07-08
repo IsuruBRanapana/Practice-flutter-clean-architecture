da902d7e42b8ee46614dfcb6f92f369072738101
isuru_r <isuru_r@epiclanka.net>
2020-07-07 19:53:10 -0700

    finish TDD to cache number trivia

--- lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
+++ lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
@@ -3,7 +3,6 @@ import 'package:cleanpractice/core/usecases/usecase.dart';
 import 'package:cleanpractice/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:cleanpractice/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 import 'package:dartz/dartz.dart';
-import 'package:equatable/equatable.dart';
 
 class GetRandomNumberTrivia extends UseCase<NumberTrivia,NoParams>{
   final NumberTriviaRepository repository;
